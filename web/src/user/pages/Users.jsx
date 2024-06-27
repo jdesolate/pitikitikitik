5f295e1253a68f35a24c0286fa19f852b80e0baa
Merv Tampus <79685609+jdesolate@users.noreply.github.com>
2024-06-27 23:45:13 +0800

    [Feature][Merv] Add User List Components and Styling

--- web/src/user/pages/Users.jsx
+++ web/src/user/pages/Users.jsx
@@ -1,5 +1,22 @@
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-  return <h1>Users</h1>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Max",
+      image: "https://picsum.photos/200/200",
+      pitiks: 3,
+    },
+    {
+      id: "u2",
+      name: "Manu",
+      image: "https://picsum.photos/200/200",
+      pitiks: 2,
+    },
+  ];
+
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
